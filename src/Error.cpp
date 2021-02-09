ca30e9b0a7e5ed7c0c5acc4839b2d56f1378d0b4
Warwick <warwick.l.e.new@gmail.com>
2021-02-09 11:06:43 +0000

    Error library now logs all errors before throw
    mainly done as I don't know how to fix throw for my os

--- src/Error.cpp
+++ src/Error.cpp
@@ -1,9 +1,18 @@
 #include "Error.h"
 
 Error::Error(std::string location) { object = location; }
-void Error::crash(std::string msg) { throw object + ": " + msg; }
-void Error::crash(std::string reason, std::string msg) {
-  throw object + ": " + reason + ": " + msg;
+void Error::crash(std::string msg) {
+  log("Error: " + msg);
+  throw object + ": \n" + msg;
 }
-void Error::warn(std::string msg) { throw object + ": " + msg; }
-void Error::log(std::string msg) { throw object + ": " + msg; }
+void Error::crash(std::string reason, std::string msg) {
+  log("Error: " + reason + ": \n" + msg);
+  throw object + ": \n" + reason + ": \n" + msg;
+}
+void Error::warn(std::string msg) {
+  log("Warining: " + msg);
+  // throw object + ": " + msg;
+}
+void Error::log(std::string msg) {
+  std::cout << object << ": \n" << msg << std::endl;
+}
