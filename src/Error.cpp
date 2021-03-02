0e7bb764a6d8c20c9c2f0a40df0bf6c947c08add
Warwick <warwick.l.e.new@gmail.com>
2021-03-02 17:25:37 +0000

    First version of glsl file reader is in

--- src/Error.cpp
+++ src/Error.cpp
@@ -3,11 +3,11 @@
 Error::Error(std::string location) { object = location; }
 void Error::crash(std::string msg) {
   log("Error: " + msg);
-  throw object + ": \n" + msg;
+  throw std::string(object + ": \n" + msg);
 }
 void Error::crash(std::string reason, std::string msg) {
   log("Error: " + reason + ": \n" + msg);
-  throw object + ": \n" + reason + ": \n" + msg;
+  throw std::string(object + ": \n" + reason + ": \n" + msg);
 }
 void Error::warn(std::string msg) {
   log("Warining: " + msg);
