19ad21dd7ef0b06d749e87cec0c41477e83d4307
Warwick <warwick.l.e.new@gmail.com>
2022-01-27 15:30:40 +0000

    I dunno why it doesn't work currently be we should have a working camera

--- src/PlayerCamera.cpp
+++ src/PlayerCamera.cpp
@@ -7,6 +7,51 @@ PlayerCamera::PlayerCamera() {
 
 PlayerCamera::~PlayerCamera() {}
 
-void PlayerCamera::tick() {}
+void PlayerCamera::tick() {
+  pos.vectorPos = glm::vec4(0.0f, 0.0f, 5.0f, 1.0f);
+  // Get the mouse movement
+  int mouseX, mouseY;
+  SDL_GetRelativeMouseState(&mouseX, &mouseY);
 
-glm::mat4 PlayerCamera::getMVP() {}
+  // Calculate rotation
+  pos.yaw -= mouseX * mouseSensitivity;
+  pos.pitch -= mouseX * mouseSensitivity;
+
+  // Stop camera from looking too far up/down
+  const float maxPitch = glm::radians(89.0f);
+  if (pos.pitch > maxPitch)
+    pos.pitch = maxPitch;
+  if (pos.pitch < -maxPitch)
+    pos.pitch = -maxPitch;
+
+  glm::vec4 playerForward(0.0f, 0.0f, -1.0f, 0.0f);
+  glm::mat4 playerRotation;
+  // Rotate yaw
+  playerRotation =
+      glm::rotate(playerRotation, pos.yaw, glm::vec3(0.0f, 1.0f, 0.0f));
+  // Rotate pitch
+  playerRotation =
+      glm::rotate(playerRotation, pos.pitch, glm::vec3(1.0f, 0.0f, 0.0f));
+  playerForward = playerRotation * playerForward;
+
+  // until we add keyboard stuff set the position to a static value
+
+  // create MVP
+  glm::mat4 view = glm::lookAt(
+      glm::vec3(pos.vectorPos),
+      glm::vec3(pos.vectorPos +
+                playerForward /* glm::vec4(0.0f, 0.0f, -1.0f, 0.0f) */),
+      glm::vec3(0.0f, 1.0f, 0.0f));
+
+  // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1
+  // unit <-> 100 units
+  float width = 800;
+  float height = 600;
+  glm::mat4 projection = glm::perspective(
+      glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+  glm::mat4 transform = glm::mat4(1.0f);
+
+  MVP = projection * view * transform;
+}
+
+glm::mat4 PlayerCamera::getMVP() { return MVP; }
--- src/PlayerCamera.h
+++ src/PlayerCamera.h
@@ -1,22 +1,26 @@
 #pragma once
+#include "Error.h"
 #include <SDL2/SDL_mouse.h>
 #include <SDL2/SDL_stdinc.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
 #include <string>
 
 class PlayerCamera {
 private:
+  // Error reporting class
+  Error error = Error("PlayerCamera");
   // Constants for speed and sensitivity
-  const float movementSpeed = 0.05f;
-  const float mouseSensitivity = 0.05f;
+  const float movementSpeed = 0.005f;
+  const float mouseSensitivity = 0.005f;
 
   // Player position
   struct position {
-    glm::vec4 location;
+    glm::vec4 vectorPos;
     float pitch;
     float yaw;
-  };
+  } pos;
 
   // Mouse position
   int mouseX, mouseY;
