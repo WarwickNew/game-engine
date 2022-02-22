65e7447fe3a328b3cdad0f0ce6a7a09476a4ff73
Warwick <warwick.l.e.new@gmail.com>
2022-02-22 14:42:07 +0000

    Added mesh class (unfinished)

---
+++ src/Mesh.cpp
@@ -0,0 +1,10 @@
+#include "Mesh.h"
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices,
+           std::vector<Texture> textures) {
+  this->vertices = vertices;
+  this->indices = indices;
+  this->textures = textures;
+
+  setupMesh();
+}
---
+++ src/Mesh.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Error.h"
+#include "ShaderLoader.h"
+#include <glm/glm.hpp>
+#include <iostream>
+#include <vector>
+
+// Define some useful structures to be used in the mesh object
+struct Vertex {
+  glm::vec3 Position;
+  glm::vec3 Normal;
+  glm::vec2 TexCoords;
+};
+struct Texture {
+  unsigned int id;
+  std::string type;
+};
+
+class Mesh {
+private:
+  Error error = Error("Mesh");
+
+  unsigned int VAO, VBO, EBO;
+  void setupMesh();
+
+public:
+  std::vector<Vertex> vertices;
+  std::vector<unsigned int> indices;
+  std::vector<Texture> textures;
+
+  void Draw(ShaderLoader &shader);
+  Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices,
+       std::vector<Texture> textures);
+};
